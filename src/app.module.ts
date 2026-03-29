014a83c99d08df9f856a14bb68c7811bdee0566b
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-03-29 13:57:50 -0300

    feat: update AppModule to include ControllersModule and UsecasesProxyModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
+import { ControllersModule } from '@infra/controllers/controllers.module'
+
 import { LoggerModule } from '@infra/logger/logger.module'
+import { UsecasesProxyModule } from '@infra/usecases-proxy/usecases-proxy.module'
 
 @Module({
-  imports: [EnvironmentModule, LoggerModule],
-  controllers: [],
-  providers: [],
+  imports: [ControllersModule, EnvironmentModule, LoggerModule, UsecasesProxyModule.register()],
 })
 export class AppModule {}
