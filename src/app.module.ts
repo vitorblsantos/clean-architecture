7bf6d16ac9420bed80cf49434d596a541225d71d
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-01 01:42:53 -0300

    feat: integrate ThrottlerModule and configure global ThrottlerGuard in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
 import { ControllersModule } from '@infra/controllers/controllers.module'
@@ -7,6 +9,13 @@ import { LoggerModule } from '@infra/logger/logger.module'
 import { UsecasesProxyModule } from '@infra/usecases-proxy/usecases-proxy.module'
 
 @Module({
-  imports: [ControllersModule, EnvironmentModule, LoggerModule, UsecasesProxyModule.register()],
+  imports: [
+    ControllersModule,
+    EnvironmentModule,
+    LoggerModule,
+    ThrottlerModule.forRoot([{ ttl: 60_000, limit: 10 }]),
+    UsecasesProxyModule.register(),
+  ],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
