da896b0acf788d0dcd9e9c5b8766ef799c0de0cb
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-01 02:02:53 -0300

    feat: enhance ControllersModule by adding CQRS support and integrating Logger and Repositories modules

--- src/infra/controllers/controllers.module.ts
+++ src/infra/controllers/controllers.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common'
+import { CqrsModule } from '@nestjs/cqrs'
 import { TerminusModule } from '@nestjs/terminus'
 
-import { UsecasesProxyModule } from '../usecases-proxy/usecases-proxy.module'
+import { LoggerModule } from '@infra/logger/logger.module'
+import { RepositoriesModule } from '@infra/repositories/repositories.module'
+
 import { HealthController } from './health/health.controller'
 import { HelloController } from './hello/hello.controller'
+import { SayHelloHandler } from './hello/sayHello.handler'
 
 @Module({
-  imports: [TerminusModule, UsecasesProxyModule.register()],
+  imports: [CqrsModule, LoggerModule, RepositoriesModule, TerminusModule],
   controllers: [HealthController, HelloController],
+  providers: [SayHelloHandler],
 })
 export class ControllersModule {}
