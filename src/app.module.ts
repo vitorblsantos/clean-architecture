a29e21b9a6f570f9b0dde90386a4a39b061ba462
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-01 02:02:41 -0300

    refactor: streamline imports in AppModule for improved readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,10 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
 import { ControllersModule } from '@infra/controllers/controllers.module'
-
 import { LoggerModule } from '@infra/logger/logger.module'
-import { UsecasesProxyModule } from '@infra/usecases-proxy/usecases-proxy.module'
 
 @Module({
-  imports: [
-    ControllersModule,
-    EnvironmentModule,
-    LoggerModule,
-    ThrottlerModule.forRoot([{ limit: 10, ttl: 60_000 }]),
-    UsecasesProxyModule.register(),
-  ],
+  imports: [ControllersModule, EnvironmentModule, LoggerModule, ThrottlerModule.forRoot([{ limit: 10, ttl: 60_000 }])],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
