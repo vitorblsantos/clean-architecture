8d5adc2cd83ee8339851b706436c70f1e64498a3
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-07 03:49:22 -0300

    feat: update imports in AppModule to use ApiModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 
+import { ApiModule } from '@api/api.module'
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
-import { ControllersModule } from '@infra/controllers/controllers.module'
 import { LoggerModule } from '@infra/logger/logger.module'
 
 @Module({
-  imports: [ControllersModule, EnvironmentModule, LoggerModule, ThrottlerModule.forRoot([{ limit: 10, ttl: 60_000 }])],
+  imports: [ApiModule, EnvironmentModule, LoggerModule, ThrottlerModule.forRoot([{ limit: 10, ttl: 60_000 }])],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
