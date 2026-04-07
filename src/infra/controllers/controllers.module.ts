babba8f479fdfd8e91f783c9e3a7123c1f97202e
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-06 23:43:49 -0300

    feat: add UseCasesModule to ControllersModule imports

--- src/infra/controllers/controllers.module.ts
+++ src/infra/controllers/controllers.module.ts
@@ -2,16 +2,15 @@ import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
 import { TerminusModule } from '@nestjs/terminus'
 
+import { UseCasesModule } from '@app/usecases.module'
 import { LoggerModule } from '@infra/logger/logger.module'
 import { RepositoriesModule } from '@infra/repositories/repositories.module'
 
 import { HealthController } from './health/health.controller'
 import { HelloController } from './hello/hello.controller'
-import { SayHelloHandler } from './hello/sayHello.handler'
 
 @Module({
-  imports: [CqrsModule, LoggerModule, RepositoriesModule, TerminusModule],
+  imports: [CqrsModule, LoggerModule, RepositoriesModule, TerminusModule, UseCasesModule],
   controllers: [HealthController, HelloController],
-  providers: [SayHelloHandler],
 })
 export class ControllersModule {}
