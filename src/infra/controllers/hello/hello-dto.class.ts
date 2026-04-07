7de57bb6a3d935002e57d028241359f715e58ee4
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-07 03:56:18 -0300

    feat: remove HelloDto as part of code cleanup

--- src/infra/controllers/hello/hello-dto.class.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString } from 'class-validator'
-
-export class HelloDto {
-  @ApiProperty({ required: true, default: 'd290f1ee-6c54-4b01-90e6-d701748f0851' })
-  @IsNotEmpty()
-  @IsString()
-  readonly id: string
-}
