32fa0a3b54b66e3a9c4277b146f2ab5e849c84e2
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-07 04:25:43 -0300

    fix: update migration to create and insert into profiles table instead of profile_model

--- src/infra/persistence/typeorm/migrations/1775563200000-add-profile.ts
+++ src/infra/persistence/typeorm/migrations/1775563200000-add-profile.ts
@@ -5,14 +5,14 @@ export class AddProfile1775563200000 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "public"."profile_model" ("id" character varying NOT NULL, "name" character varying NOT NULL, "lastname" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0fb22df1705f67ccc5b43ac1433" PRIMARY KEY ("id"), CONSTRAINT "UQ_0fb22df1705f67ccc5b43ac1433" UNIQUE ("id"))`,
+      `CREATE TABLE "public"."profiles" ("id" character varying NOT NULL, "name" character varying NOT NULL, "lastname" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_0fb22df1705f67ccc5b43ac1433" PRIMARY KEY ("id"), CONSTRAINT "UQ_0fb22df1705f67ccc5b43ac1433" UNIQUE ("id"))`,
     )
     await queryRunner.query(
-      `INSERT INTO "public"."profile_model" ("id", "name", "lastname") VALUES ('d290f1ee-6c54-4b01-90e6-d701748f0851', 'John', 'Doe')`,
+      `INSERT INTO "public"."profiles" ("id", "name", "lastname") VALUES ('d290f1ee-6c54-4b01-90e6-d701748f0851', 'John', 'Doe')`,
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE "public"."profile_model"`)
+    await queryRunner.query(`DROP TABLE "public"."profiles"`)
   }
 }
