6c69c0ec14d3c2b8d4b8f5e0a18de29e88f8c2a6
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-07 04:20:15 -0300

    fix: update TypeORM configuration to use ProfileModel and correct migrations

--- src/infra/config/typeorm/typeorm.module.ts
+++ src/infra/config/typeorm/typeorm.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
 
-import { addUser1774808948434 } from '@database/migrations/index.migrations'
-
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
 import { EnvironmentService } from '@infra/config/environment/environment.service'
-import { User } from '@infra/models/profile/profile.model'
+import { ProfileModel } from '@infra/models/profile/profile.model'
+import { typeOrmMigrations } from '@infra/persistence/typeorm/typeorm.migrations'
 
 export const getTypeOrmModuleOptions = (config: EnvironmentService): TypeOrmModuleOptions => ({
   type: 'postgres',
@@ -14,11 +13,11 @@ export const getTypeOrmModuleOptions = (config: EnvironmentService): TypeOrmModu
   username: config.getDatabaseUser(),
   password: config.getDatabasePassword(),
   database: config.getDatabaseName(),
-  entities: [User],
+  entities: [ProfileModel],
   synchronize: config.getDatabaseSync(),
   schema: config.getDatabaseSchema(),
   migrationsRun: true,
-  migrations: [addUser1774808948434],
+  migrations: typeOrmMigrations,
   // ssl: {
   //   rejectUnauthorized: false,
   // },
