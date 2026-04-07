e4ad249b108628f60e4a31aa067e4544ebe7d4aa
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-07 04:18:37 -0300

    feat: enhance ProfileModule imports for improved functionality

--- src/application/profile/profile.module.ts
+++ src/application/profile/profile.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { CreateProfileHandler } from '@app/profile/command/handler/create-profile.handler'
 import { ProfileService } from '@app/services/profile/profile.service'
 import { ProfileDomainService } from '@domain/services/profile/profile.service'
+import { LoggerModule } from '@infra/logger/logger.module'
+import { ProfileModel } from '@infra/models/profile/profile.model'
 import { ProfileRepository } from '@infra/repositories/profile/profile.repository'
 
 export const CommandHandlers = [CreateProfileHandler]
 export const Sagas = []
 
 @Module({
-  imports: [CqrsModule],
+  imports: [CqrsModule, LoggerModule, TypeOrmModule.forFeature([ProfileModel])],
   providers: [
     ProfileService,
     ProfileDomainService,
