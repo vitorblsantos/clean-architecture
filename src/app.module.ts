d5dc1d7845b3157060dcd230fb5b8e95411b85ab
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-07 04:18:16 -0300

    feat: reorganize imports in app.module.ts for improved readability

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
 
 import { ApiModule } from '@api/api.module'
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
+import { TypeOrmConfigModule } from '@infra/config/typeorm/typeorm.module'
 import { LoggerModule } from '@infra/logger/logger.module'
 
 @Module({
-  imports: [ApiModule, EnvironmentModule, LoggerModule, ThrottlerModule.forRoot([{ limit: 10, ttl: 60_000 }])],
+  imports: [
+    EnvironmentModule,
+    TypeOrmConfigModule,
+    ApiModule,
+    LoggerModule,
+    ThrottlerModule.forRoot([{ limit: 10, ttl: 60_000 }]),
+  ],
   providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
