2ab74dd543533bd746049cec1c8e649cd40eecd9
Vitor Barbosa de Lima <vitorblsantos@gmail.com>
2026-04-14 11:44:53 -0300

    fix: update TypeORM configuration to use .ts files for entities and enable synchronization based on environment variable

--- src/infra/config/typeorm/typeorm.config.ts
+++ src/infra/config/typeorm/typeorm.config.ts
@@ -5,20 +5,14 @@ if (process.env.NODE_ENV === 'local') dotenv.config({ path: './env' })
 
 const config: DataSourceOptions = {
   database: process.env.DATABASE_NAME,
-  entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+  entities: [__dirname + '/../../**/*.entity{.ts}'],
   host: process.env.DATABASE_HOST,
-  migrationsRun: false,
-  migrations: ['src/infra/persistence/typeorm/migrations/*{.ts,.js}'],
-  migrationsTableName: 'migrations',
   password: process.env.DATABASE_PASSWORD,
   port: parseInt(process.env.DATABASE_PORT!),
   type: 'postgres',
-  synchronize: false,
+  synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
   schema: process.env.DATABASE_SCHEMA,
   username: process.env.DATABASE_USER,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
 }
 
 export default config
