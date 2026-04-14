a4d07eba814299afbd6903feebff4c507c356467
Vitor Barbosa de Lima <vitorblsantos@gmail.com>
2026-04-14 12:06:15 -0300

    feat: add findAll method to IProfileRepository for retrieving all profiles

--- src/domain/interfaces/repositories/profile-repository.interface.ts
+++ src/domain/interfaces/repositories/profile-repository.interface.ts
@@ -2,4 +2,5 @@ import { ProfileEntity } from '@domain/entities/profile.entity'
 
 export interface IProfileRepository {
   create(profile: ProfileEntity): Promise<ProfileEntity>
+  findAll(): Promise<ProfileEntity[]>
 }
