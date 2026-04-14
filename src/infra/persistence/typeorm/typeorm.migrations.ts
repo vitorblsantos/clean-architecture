d323a4b05a88b16682fda24b9c726bae3e68b982
Vitor Barbosa de Lima <vitorblsantos@gmail.com>
2026-04-14 11:45:00 -0300

    chore: remove obsolete profile migration and TypeORM migration reference

--- src/infra/persistence/typeorm/typeorm.migrations.ts
+++
@@ -1,5 +0,0 @@
-import type { MigrationInterface } from 'typeorm'
-
-import { AddProfile1775563200000 } from './migrations/1775563200000-add-profile'
-
-export const typeOrmMigrations: (new () => MigrationInterface)[] = [AddProfile1775563200000]
