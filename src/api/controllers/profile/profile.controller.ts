b89ea3c8a93ec8d359b233cf339bca6a4f3c2da2
Vitor Barbosa de Lima <vitorblsantos@gmail.com>
2026-04-14 11:58:39 -0300

    feat: add endpoint to retrieve all profiles and update profile creation operation

--- src/api/controllers/profile/profile.controller.ts
+++ src/api/controllers/profile/profile.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger'
 
 import { CreateProfileDto } from '@api/dto/profile/profile.dto'
 import { ProfileService } from '@app/services/profile/profile.service'
+import { ProfileEntity } from '@domain/entities/profile.entity'
 
 @ApiTags('profile')
 @Controller({
@@ -12,10 +13,16 @@ import { ProfileService } from '@app/services/profile/profile.service'
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
 
+  @Get('/')
+  @ApiOperation({ description: 'Get all profiles' })
+  async findAll(): Promise<ProfileEntity[]> {
+    return await this.profileService.findAll()
+  }
+
   @Post('/')
   @ApiBody({ type: CreateProfileDto })
-  @ApiOperation({ description: 'say hello' })
-  async login(@Body() body: CreateProfileDto) {
+  @ApiOperation({ description: 'Create a new profile' })
+  async create(@Body() body: CreateProfileDto): Promise<ProfileEntity> {
     return await this.profileService.create(body)
   }
 }
