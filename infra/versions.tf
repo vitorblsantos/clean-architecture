788f233bfca3e1520c42eb9e43b4b9f95ddf18e4
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-14 23:03:21 -0300

    feat: add Terraform configuration for Google provider and project data

---
+++ infra/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_project" "current" {
+  project_id = var.project_id
+}
