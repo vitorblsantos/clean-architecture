146a50293d70906f85aed36dc355a096f3c00523
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-14 23:02:41 -0300

    feat: add Terraform configuration for required Google APIs

---
+++ infra/apis.tf
@@ -0,0 +1,15 @@
+locals {
+  required_apis = [
+    "run.googleapis.com",
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "required" {
+  for_each = toset(local.required_apis)
+
+  project            = var.project_id
+  service            = each.key
+  disable_on_destroy = false
+}
