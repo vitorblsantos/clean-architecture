e8339bd00913ea853fb283d7eae4107dfe9d6bb5
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-27 04:13:34 -0300

    feat: add EnqueueProfileUpdateCommand and UpdateProfileHandler for processing profile updates

---
+++ src/application/profile/command/handler/update.handler.ts
@@ -0,0 +1,14 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
+
+import { UpdateProfileCommand } from '@app/profile/command/update.command'
+import { ProfileService } from '@app/services/profile/profile.service'
+
+@CommandHandler(UpdateProfileCommand)
+export class UpdateProfileHandler implements ICommandHandler<UpdateProfileCommand, void> {
+  constructor(private readonly profileService: ProfileService) {}
+
+  async execute(command: UpdateProfileCommand): Promise<void> {
+    const { id, name, lastname } = command
+    await this.profileService.update({ id, name, lastname })
+  }
+}
