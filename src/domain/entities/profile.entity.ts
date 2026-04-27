c8ce7856fcf04bec5fa383dc05135236f60e573b
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-26 23:47:22 -0300

    chore: update ProfileEntity properties to use definite assignment assertions

--- src/domain/entities/profile.entity.ts
+++ src/domain/entities/profile.entity.ts
@@ -1,7 +1,7 @@
 export class ProfileEntity {
-  createdAt: Date
-  id: string
-  lastname: string
-  name: string
-  updatedAt: Date
+  createdAt!: Date
+  id!: string
+  lastname!: string
+  name!: string
+  updatedAt!: Date
 }
