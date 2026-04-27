bd733225c81412d9caa7bd6d8eea980b71dc5400
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-27 01:50:41 -0300

    chore: update enqueue method in IProfileService to return void

--- src/domain/interfaces/services/profile-service.interfaces.ts
+++ src/domain/interfaces/services/profile-service.interfaces.ts
@@ -2,7 +2,7 @@ import { ProfileEntity } from '@domain/entities/profile.entity'
 
 export interface IProfileService {
   create(profile: ProfileEntity): Promise<ProfileEntity>
-  enqueue(payload: Partial<ProfileEntity>): Promise<ProfileEntity>
+  enqueue(payload: Partial<ProfileEntity>): Promise<void>
   findAll(): Promise<ProfileEntity[]>
   findById(id: string): Promise<ProfileEntity>
   update(id: string, payload: Partial<ProfileEntity>): Promise<ProfileEntity>
