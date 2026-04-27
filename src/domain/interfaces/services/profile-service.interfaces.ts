4964bad7278d0100386cfe5225d632921af43546
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-04-26 23:46:38 -0300

    chore: add methods to IProfileService interface for enqueue, findById, and update operations

--- src/domain/interfaces/services/profile-service.interfaces.ts
+++ src/domain/interfaces/services/profile-service.interfaces.ts
@@ -2,5 +2,8 @@ import { ProfileEntity } from '@domain/entities/profile.entity'
 
 export interface IProfileService {
   create(profile: ProfileEntity): Promise<ProfileEntity>
+  enqueue(payload: Partial<ProfileEntity>): Promise<ProfileEntity>
   findAll(): Promise<ProfileEntity[]>
+  findById(id: string): Promise<ProfileEntity>
+  update(id: string, payload: Partial<ProfileEntity>): Promise<ProfileEntity>
 }
