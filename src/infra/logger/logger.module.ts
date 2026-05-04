51acfb7647113951e918a910b74b63ddee128b1a
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-04 08:58:07 -0300

    fix: update import path for LoggerService to use alias

--- src/infra/logger/logger.module.ts
+++ src/infra/logger/logger.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 
-import { LoggerService } from 'src/application/services/logger/logger.service'
+import { LoggerService } from '@app/services/logger/logger.service'
 import { EnvironmentModule } from '@infra/config/environment/environment.module'
 
 @Module({
