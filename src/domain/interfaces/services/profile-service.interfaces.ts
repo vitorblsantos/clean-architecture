a53efe416be4455f2bfeb14ee61705206751aba7
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-04 08:58:52 -0300

    feat: add delete method to IProfileService interface

--- src/domain/interfaces/services/profile-service.interfaces.ts
+++ src/domain/interfaces/services/profile-service.interfaces.ts
@@ -2,6 +2,7 @@ import { ProfileEntity } from '@domain/entities/profile.entity'
 
 export interface IProfileService {
   create(profile: ProfileEntity): Promise<ProfileEntity>
+  delete(id: string): Promise<void>
   enqueue(payload: Partial<ProfileEntity>): Promise<void>
   findAll(): Promise<ProfileEntity[]>
   findById(id: string): Promise<ProfileEntity>
