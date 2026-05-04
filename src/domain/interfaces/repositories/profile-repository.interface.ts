cbdfb8700eee01f3ea8812b86f005746c45d5653
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-04 08:58:43 -0300

    feat: add delete method to IProfileRepository interface

--- src/domain/interfaces/repositories/profile-repository.interface.ts
+++ src/domain/interfaces/repositories/profile-repository.interface.ts
@@ -2,6 +2,7 @@ import { ProfileEntity } from '@domain/entities/profile.entity'
 
 export interface IProfileRepository {
   create(profile: ProfileEntity): Promise<ProfileEntity>
+  delete(id: string): Promise<void>
   findAll(): Promise<ProfileEntity[]>
   findById(id: string): Promise<ProfileEntity>
   update(id: string, payload: Partial<Omit<ProfileEntity, 'id'>>): Promise<ProfileEntity>
