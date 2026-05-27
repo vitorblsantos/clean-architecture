c61218608001dffab60cba1b0e3a8fa01c07168a
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-27 00:23:35 -0300

    refactor: update ApiModule to use ProfilesController and clean up unused code

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,30 +1,17 @@
-import { Module, Provider } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { CqrsModule } from '@nestjs/cqrs'
 import { TerminusModule } from '@nestjs/terminus'
 
 import { HealthController } from '@api/controllers/health/health.controller'
-import { ProfileController } from '@api/controllers/profile/profile.controller'
+import { ProfilesController } from '@api/controllers/profiles/profiles.controller'
 
-import { AppModule } from '@app/application.module'
-
-import { EEnvironment } from '@domain/interfaces/enums/environment.enum'
+import { AppModule } from '@app/app.module'
+import { EnvironmentModule } from '@infra/environment/environment.module'
 import { KafkaHealthIndicator } from '@infra/health/kafka.health'
 
-const healthProviders: Provider[] =
-  process.env.NODE_ENV === EEnvironment.Local
-    ? [KafkaHealthIndicator]
-    : [
-        {
-          provide: KafkaHealthIndicator,
-          useValue: {
-            isHealthy: (key: string) => ({ [key]: { status: 'up', skipped: true } }),
-          },
-        },
-      ]
-
 @Module({
-  imports: [CqrsModule, AppModule, TerminusModule],
-  controllers: [HealthController, ProfileController],
-  providers: [...healthProviders],
+  imports: [AppModule, CqrsModule, EnvironmentModule, TerminusModule],
+  controllers: [HealthController, ProfilesController],
+  providers: [KafkaHealthIndicator],
 })
 export class ApiModule {}
