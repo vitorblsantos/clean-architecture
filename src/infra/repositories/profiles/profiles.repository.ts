f6d9ea03df0f9147ab0b1b1d3016edcf8cf5f507
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-27 00:33:01 -0300

    feat: implement ProfilesRepository for profile data management

--- src/infra/repositories/profiles/profiles.repository.ts
+++ src/infra/repositories/profiles/profiles.repository.ts
@@ -2,19 +2,19 @@ import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
-import { ProfileEntity } from '@domain/entities/profile.entity'
-import { IProfileRepository } from '@domain/interfaces/repositories/profile-repository.interface'
+import { ProfilesEntity } from '@domain/entities/profiles/profiles.entity'
+import { IProfilesRepository } from '@domain/interfaces/repositories/profiles-repository.interface'
 
-import { ProfileModel } from '@infra/models/profile/profile.model'
+import { ProfilesModel } from '@infra/models/profiles/profiles.model'
 
 @Injectable()
-export class ProfileRepository implements IProfileRepository {
+export class ProfilesRepository implements IProfilesRepository {
   constructor(
-    @InjectRepository(ProfileModel)
-    private readonly profileRepository: Repository<ProfileModel>,
+    @InjectRepository(ProfilesModel)
+    private readonly profileRepository: Repository<ProfilesModel>,
   ) {}
 
-  async create(profile: ProfileEntity): Promise<ProfileEntity> {
+  async create(profile: ProfilesEntity): Promise<ProfilesEntity> {
     return await this.profileRepository.save(profile)
   }
 
@@ -23,11 +23,11 @@ export class ProfileRepository implements IProfileRepository {
     return
   }
 
-  async findAll(): Promise<ProfileEntity[]> {
+  async findAll(): Promise<ProfilesEntity[]> {
     return await this.profileRepository.find()
   }
 
-  async findById(id: string): Promise<ProfileEntity> {
+  async findById(id: string): Promise<ProfilesEntity> {
     const profile = await this.profileRepository.findOne({ where: { id } })
 
     if (!profile) throw new NotFoundException(`Profile with id ${id} not found`)
@@ -35,7 +35,7 @@ export class ProfileRepository implements IProfileRepository {
     return profile
   }
 
-  async update(id: string, payload: Partial<Omit<ProfileEntity, 'id'>>): Promise<ProfileEntity> {
+  async update(id: string, payload: Partial<Omit<ProfilesEntity, 'id'>>): Promise<ProfilesEntity> {
     await this.profileRepository.update({ id }, payload)
     return await this.findById(id)
   }
