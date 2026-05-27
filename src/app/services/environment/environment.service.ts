f4bf4f09604d93d98303c63d1a64f0d90905aa7b
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-27 00:38:21 -0300

    feat: implement EnvironmentService for environment and configuration management

---
+++ src/app/services/environment/environment.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
+import { AppConfig } from '@domain/interfaces/config/app.interface'
+import { DatabaseConfig } from '@domain/interfaces/config/database.interface'
+import { KafkaConfig } from '@domain/interfaces/config/kafka.interface'
+import { EEnvironment } from '@domain/interfaces/enums/environment.enum'
+
+@Injectable()
+export class EnvironmentService implements AppConfig, DatabaseConfig, KafkaConfig {
+  constructor(private configService: ConfigService) {}
+
+  getAppEnvironment(): EEnvironment {
+    return this.configService.getOrThrow<EEnvironment>('NODE_ENV')
+  }
+
+  getAppHost(): string {
+    return this.configService.getOrThrow<string>('HOST')
+  }
+
+  getAppPort(): number {
+    return this.configService.getOrThrow<number>('PORT')
+  }
+
+  getDatabaseHost(): string {
+    return this.configService.getOrThrow<string>('DATABASE_HOST')
+  }
+
+  getDatabasePort(): number {
+    return this.configService.getOrThrow<number>('DATABASE_PORT')
+  }
+
+  getDatabaseTimezone(): string {
+    return this.configService.getOrThrow<string>('DATABASE_TIMEZONE')
+  }
+
+  getDatabaseUser(): string {
+    return this.configService.getOrThrow<string>('DATABASE_USER')
+  }
+
+  getDatabasePassword(): string {
+    return this.configService.getOrThrow<string>('DATABASE_PASSWORD')
+  }
+
+  getDatabaseName(): string {
+    return this.configService.getOrThrow<string>('DATABASE_NAME')
+  }
+
+  getDatabaseSchema(): string {
+    return this.configService.getOrThrow<string>('DATABASE_SCHEMA')
+  }
+
+  getDatabaseSync(): boolean {
+    return this.configService.getOrThrow<boolean>('DATABASE_SYNCHRONIZE')
+  }
+
+  getKafkaBrokers(): string[] {
+    return this.configService
+      .getOrThrow<string>('KAFKA_BROKERS')
+      .split(',')
+      .map((broker) => broker.trim())
+  }
+
+  getKafkaClientId(): string {
+    return this.configService.getOrThrow<string>('KAFKA_CLIENT_ID')
+  }
+
+  getKafkaTopicProfilesSync(): string {
+    return this.configService.getOrThrow<string>('KAFKA_TOPIC_CONTACTS_SYNC')
+  }
+
+  getKafkaTopicProfilesSyncDLQ(): string {
+    return this.configService.getOrThrow<string>('KAFKA_TOPIC_CONTACTS_SYNC_DLQ')
+  }
+}
