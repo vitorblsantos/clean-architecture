9322c557ffb53c12ae4a454994c3538f21dfb901
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-27 00:35:40 -0300

    refactor: update ProfileDomainService to use ProfilesEntity for profile management

--- src/domain/services/profile/profile.service.ts
+++ src/domain/services/profile/profile.service.ts
@@ -1,18 +1,18 @@
 import { v6 as uuidv6 } from 'uuid'
-import { ProfileEntity } from '@domain/entities/profile.entity'
+import { ProfilesEntity } from '@domain/entities/profiles/profiles.entity'
 
 export class ProfileDomainService {
-  canCreateProfile(existingProfile: ProfileEntity | null): boolean {
+  canCreateProfile(existingProfile: ProfilesEntity | null): boolean {
     return !existingProfile
   }
 
-  createProfileEntity(profileData: {
-    name: ProfileEntity['name']
-    lastname: ProfileEntity['lastname']
-  }): ProfileEntity {
+  createProfilesEntity(profileData: {
+    name: ProfilesEntity['name']
+    lastname: ProfilesEntity['lastname']
+  }): ProfilesEntity {
     this.validateProfileData(profileData)
 
-    const profile: ProfileEntity = {
+    const profile: ProfilesEntity = {
       id: this.generateProfileId(),
       deletedAt: null,
       name: profileData.name,
@@ -24,22 +24,32 @@ export class ProfileDomainService {
     return profile
   }
 
-  isProfileComplete(profile: ProfileEntity): boolean {
+  isProfileComplete(profile: ProfilesEntity): boolean {
     return !!(profile.name && profile.lastname)
   }
 
-  validateProfileData(profileData: { name: ProfileEntity['name']; lastname: ProfileEntity['lastname'] }): void {
+  updateProfilesEntity(profileData: Partial<ProfilesEntity>): Partial<ProfilesEntity> {
+    const { id, name, lastname } = profileData
+
+    if (!id) throw new Error('id is required')
+    if (!name && !lastname) throw new Error('At least one of name or lastname is required to update a profile')
+    const payload = { ...profileData, updatedAt: new Date() }
+
+    return payload
+  }
+
+  validateProfileData(profileData: { name: ProfilesEntity['name']; lastname: ProfilesEntity['lastname'] }): void {
     this.validateName(profileData.name)
     this.validateLastname(profileData.lastname)
   }
 
-  validateName(name: ProfileEntity['name']): void {
+  validateName(name: ProfilesEntity['name']): void {
     if (!name || name.trim().length < 2) {
       throw new Error('Name must be at least 2 characters long')
     }
   }
 
-  validateLastname(lastname: ProfileEntity['lastname']): void {
+  validateLastname(lastname: ProfilesEntity['lastname']): void {
     if (!lastname || lastname.trim().length < 2) {
       throw new Error('Lastname must be at least 2 characters long')
     }
