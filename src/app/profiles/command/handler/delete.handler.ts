43b2f6d741cbdab8bb154247f268843caf38f8a8
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-05-27 00:27:07 -0300

    feat: add DeleteProfileHandler for handling profile deletion commands

--- src/app/profiles/command/handler/delete.handler.ts
+++ src/app/profiles/command/handler/delete.handler.ts
@@ -1,15 +1,15 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs'
 
-import { DeleteProfileCommand } from '@app/profile/command/delete.command'
-import { ProfileService } from '@app/services/profile/profile.service'
+import { DeleteProfileCommand } from '@app/profiles/command/delete.command'
+import { ProfilesService } from '@app/services/profiles/profiles.service'
 
 @CommandHandler(DeleteProfileCommand)
 export class DeleteProfileHandler implements ICommandHandler<DeleteProfileCommand, void> {
-  constructor(private readonly profileService: ProfileService) {}
+  constructor(private readonly ProfilesService: ProfilesService) {}
 
   async execute(command: DeleteProfileCommand): Promise<void> {
     const { id } = command
-    await this.profileService.delete(id)
+    await this.ProfilesService.delete(id)
     return
   }
 }
