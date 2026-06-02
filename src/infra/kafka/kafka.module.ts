e65b0b6f6ad421ad9ae70791851beed829c3e078
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-06-02 01:07:05 -0300

    refactor: update KafkaModule to provide IKafkaService interface alongside KafkaService for improved abstraction

--- src/infra/kafka/kafka.module.ts
+++ src/infra/kafka/kafka.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common'
 
-import { KafkaService } from '@app/services/kafka/kafka.service'
+import { IKafkaService } from '@domain/interfaces/kafka/kafka.interface'
 
 import { EnvironmentModule } from '@infra/environment/environment.module'
+import { KafkaService } from '@infra/kafka/kafka.service'
 import { LoggerModule } from '@infra/logger/logger.module'
 
 @Module({
   imports: [EnvironmentModule, LoggerModule],
-  providers: [KafkaService],
-  exports: [KafkaService],
+  providers: [KafkaService, { provide: IKafkaService, useExisting: KafkaService }],
+  exports: [KafkaService, IKafkaService],
 })
 export class KafkaModule {}
