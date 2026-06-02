b269987d01eaada285063fd79793fb05f11a8548
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-06-02 01:04:00 -0300

    refactor: update GetProfilesHandler to use IProfilesService for improved abstraction

--- src/app/profiles/query/handler/get-profiles.handler.ts
+++ src/app/profiles/query/handler/get-profiles.handler.ts
@@ -1,14 +1,15 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
 
 import { GetProfilesQuery } from '@app/profiles/query/get-profiles.query'
-import { ProfilesService } from '@app/services/profiles/profiles.service'
+
 import { ProfilesEntity } from '@domain/entities/profiles/profiles.entity'
+import { IProfilesService } from '@domain/interfaces/services/profile-service.interface'
 
 @QueryHandler(GetProfilesQuery)
 export class GetProfilesHandler implements IQueryHandler<GetProfilesQuery, ProfilesEntity[]> {
-  constructor(private readonly ProfilesService: ProfilesService) {}
+  constructor(private readonly profilesService: IProfilesService) {}
 
   async execute(_query: GetProfilesQuery): Promise<ProfilesEntity[]> {
-    return await this.ProfilesService.findAll()
+    return await this.profilesService.findAll()
   }
 }
