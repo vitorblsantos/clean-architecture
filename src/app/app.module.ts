47a60754c3c43eae601a9b56dd19d99fb06b1297
Vitor Barbosa de Lima Santos <vitorblsantos@gmail.com>
2026-07-01 21:32:31 -0300

    fix: remove duplicate import of LLMModule in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 
 import { LLMModule } from '@app/llm/llm.module'
 import { ProfileModule } from '@app/profiles/profiles.module'
-import { LLMModule } from '@app/llm/llm.module'
 
 @Module({
   imports: [LLMModule, ProfileModule],
